1e064973a2ad657f456a739096b79eadb12ffc9c
richardcalata <calatar6@gmail.com>
2019-01-14 16:11:47 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class App extends React.Component{
+    render() {
+
+        return(
+            <div>Hello World</div>
+        )
+    }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
